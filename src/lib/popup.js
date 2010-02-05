ebababbdbf97c3e9958a0b3c0814726ef36349c7
Constellation <utatane.tea@gmail.com>
2010-02-06 01:16:11 +0900

    * fix flavor delete

--- src/lib/popup.js
+++ src/lib/popup.js
@@ -188,8 +188,9 @@ Form.prototype = {
   save: function(){
     Object.keys(this.savers).forEach(function(key){
       var body = this.savers[key].body();
-      if((''+this.ps[key]) !== body)
-        this.ps[key] = body;
+      if(key === 'body' && this.ps[key] !== body)
+        delete ps['flavors'];
+      this.ps[key] = body;
     }, this);
     background.TBRL.Popup.contents[this.ps.itemUrl] = this.ps;
   },
