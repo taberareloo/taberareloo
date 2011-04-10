5eece23f3160298ddf8b6c7c1df4d2ecb1a63e43
Constellation <utatane.tea@gmail.com>
2011-04-11 00:56:54 +0900

    version 2.0.13
    add Upload from Cache

--- src/lib/background.js
+++ src/lib/background.js
@@ -292,7 +292,7 @@ var GlobalFileEntryCache = {
 
 var TBRL = {
   // default config
-  VERSION: '2.0.12',
+  VERSION: '2.0.13',
   ID: chrome.extension.getURL('').match(/chrome-extension:\/\/([^\/]+)\//)[1],
   Config: {
     'services': {
