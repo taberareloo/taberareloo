12ba19a336bfb3a079abe9abf53678097a6c953d
Constellation <utatane.tea@gmail.com>
2011-09-27 02:50:29 -0700

    version 2.0.28. update delicious model. close #54

--- src/lib/background.js
+++ src/lib/background.js
@@ -103,7 +103,7 @@ var GlobalFileEntryCache = { };
 
 var TBRL = {
   // default config
-  VERSION: '2.0.27',
+  VERSION: '2.0.28',
   ID: chrome.extension.getURL('').match(/chrome-extension:\/\/([^\/]+)\//)[1],
   Config: {
     'services': {
