7cdd943786d123dddf1d06426053e9d61fd4c70f
Constellation <utatane.tea@gmail.com>
2011-10-06 00:01:03 +0900

    version 2.0.33 fix Delicious

--- src/lib/background.js
+++ src/lib/background.js
@@ -103,7 +103,7 @@ var GlobalFileEntryCache = { };
 
 var TBRL = {
   // default config
-  VERSION: '2.0.32',
+  VERSION: '2.0.33',
   ID: chrome.extension.getURL('').match(/chrome-extension:\/\/([^\/]+)\//)[1],
   Config: {
     'services': {
