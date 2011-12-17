5cc65dc1dc82c87432fd05101095e79be22a0b0f
Constellation <utatane.tea@gmail.com>
2011-12-18 01:13:11 +0900

    version 2.0.46 fix YouTube Extractor

--- src/lib/background.js
+++ src/lib/background.js
@@ -103,7 +103,7 @@ var GlobalFileEntryCache = { };
 
 var TBRL = {
   // default config
-  VERSION: '2.0.45',
+  VERSION: '2.0.46',
   ID: chrome.extension.getURL('').match(/chrome-extension:\/\/([^\/]+)\//)[1],
   Config: {
     'services': {
