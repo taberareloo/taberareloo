146b1dae0fc2eb89b892d8f0648b4c259bfa0ed3
Constellation <utatane.tea@gmail.com>
2012-02-24 03:41:32 +0900

    version 2.0.58

--- src/lib/background.js
+++ src/lib/background.js
@@ -64,7 +64,7 @@ var GlobalFileEntryCache = { };
 
 var TBRL = {
   // default config
-  VERSION: '2.0.57',
+  VERSION: '2.0.58',
   ID: chrome.extension.getURL('').match(/chrome-extension:\/\/([^\/]+)\//)[1],
   Config: {
     'services': {
