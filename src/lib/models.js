12e29d3059adeb6d993e6583ad7a79aa151c1383
Constellation <utatane.tea@gmail.com>
2012-04-24 22:41:36 +0900

    retry on https in Local model

--- src/lib/models.js
+++ src/lib/models.js
@@ -612,7 +612,27 @@ Models.register({
             windowType: 'normal'
           }, function (tabs) {
             if (tabs.length === 0) {
-              that.queue.push(url);
+              chrome.tabs.query({
+                url: 'https://*/*',
+                active: true,
+                highlighted: true,
+                windowType: 'normal'
+              }, function (tabs) {
+                if (tabs.length === 0) {
+                  chrome.tabs.query({
+                    url: 'http://*/*',
+                    windowType: 'normal'
+                  }, function (tabs) {
+                    if (tabs.length === 0) {
+                      that.queue.push(url);
+                      return;
+                    }
+                    ok(tabs[0]);
+                  });
+                  return;
+                }
+                ok(tabs[0]);
+              });
               return;
             }
             ok(tabs[0]);
