a18a183ff562eeb87d89da9678bc8ba0d7b7a13a
Constellation <utatane.tea@gmail.com>
2012-04-24 21:07:53 +0900

    remove HTML Notification code (but not used)

--- src/lib/notifications.js
+++
@@ -1,8 +0,0 @@
-(function(){
-  var query = queryHash(location.search);
-  chrome.extension.sendRequest({
-    request: 'notifications',
-    content: query['id']
-  }, function(data){
-  });
-})();
