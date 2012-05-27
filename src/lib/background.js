e6a06646ed2b85c44e84d2f5c7a2626a02a97a3c
Constellation <utatane.tea@gmail.com>
2012-05-27 21:19:18 +0900

    Move getFinalUrl from utils.js to background.js

--- src/lib/background.js
+++ src/lib/background.js
@@ -479,3 +479,63 @@ chrome.extension.onRequest.addListener(function(req, sender, func) {
     handler.apply(this, arguments);
   }
 });
+
+/**
+ * URLのリダイレクト先を取得する
+ *
+ * @param {String} url
+ * @return {Deferred} リダイレクト先のURLが返される リダイレイクトしない場合はもとのURL
+ */
+var getFinalUrl = (function() {
+  if (!chrome.webRequest) {
+    return null;
+  }
+
+  var redirects = {};
+  var threads = 0;
+
+  // リダイレクトを記録しておく
+  chrome.webRequest.onBeforeRedirect.addListener(function(detail) {
+    redirects[detail.url] = detail.redirectUrl;
+  }, {
+    urls: [
+      "http://*/*",
+      "https://*/*",
+    ],
+  }, []);
+
+  // 暇そうなときにキャッシュ削除
+  // 10 minutes
+  setInterval(function() {
+    if (threads == 0) {
+      redirects = {};
+    }
+  }, 60 * 1000 * 10);
+
+  return function getFinalUrl(url) {
+    var self = this;
+    var ret = new Deferred();
+
+    // キャッシュにあればすぐに返す
+    if (redirects[url]) {
+      setTimeout(function() {
+        ret.callback(redirects[url]);
+      }, 0, {});
+      return ret;
+    }
+
+    // URLにリクエスト送って調べる
+    threads++;
+    request(url, {
+      method: 'HEAD'
+    }).addBoth(function() {
+      threads--;
+      if (redirects[url]) {
+        ret.callback(redirects[url]);
+      } else {
+        ret.callback(url);
+      }
+    });
+    return ret;
+  };
+})();
