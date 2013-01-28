cf333856b48528f425faf7e81284c48e4e8e49b7
YungSang <yungsang@gmail.com>
2013-01-27 23:16:16 -0800

    fix "MAX_FILE_SIZE":"undefined\n\n10485760"

--- src/lib/models.js
+++ src/lib/models.js
@@ -277,7 +277,12 @@ var Tumblr = {
           if (!value) {
             return;
           }
-          form[name] += '\n\n' + value;
+          if (form[name]) {
+            form[name] += '\n\n' + value;
+          }
+          else {
+            form[name] = value;
+          }
         });
         that.appendTags(form, ps);
         return that.postForm(function(){
@@ -299,7 +304,12 @@ var Tumblr = {
             value.indexOf('http://') !== 0) {
           return;
         }
-        form[name] += '\n\n' + value;
+        if (form[name]) {
+          form[name] += '\n\n' + value;
+        }
+        else {
+          form[name] = value;
+        }
       });
 
       this.appendTags(form, ps);
