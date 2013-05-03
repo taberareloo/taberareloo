17050fc1dc88f19d0a6437940849cc621c11386d
Yusuke Suzuki <utatane.tea@gmail.com>
2013-05-04 00:17:17 +0900

    Use global eval

--- src/lib/sandbox.js
+++ src/lib/sandbox.js
@@ -1,13 +1,19 @@
-onmessage = function(req) {
-  switch (req.data.action) {
-  case 'evalJSON':
-    var res = {
-      seq : req.data.seq
-    };
-    res.json = eval('(' + req.data.value + ')');
-    top.postMessage(res, '*');
-    break;
-  default:
-  }
-};
+(function () {
+  'use strict';
+
+  var globalEval = ('global', eval);
+
+  window.onmessage = function(req) {
+    switch (req.data.action) {
+    case 'evalJSON':
+      var res = {
+        seq : req.data.seq
+      };
+      res.json = globalEval('(' + req.data.value + ')');
+      top.postMessage(res, '*');
+      break;
+    default:
+    }
+  };
+}());
 /* vim: set sw=2 ts=2 et tw=80 : */
